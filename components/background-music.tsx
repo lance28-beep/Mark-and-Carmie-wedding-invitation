a77631b53c398e4404bfba9d30af92c78880c7e7
Lance Valle <vallelance8@gmail.com>
2026-01-31 22:10:05 +0800

    update

--- components/background-music.tsx
+++ components/background-music.tsx
@@ -47,7 +47,7 @@ const BackgroundMusic = () => {
     <audio
       ref={audioRef}
       // Use an encoded URI to avoid issues with spaces/parentheses on some mobile browsers
-      src={encodeURI("/background_music/I Get To Love You - Ruelle  Wedding Dance ONLINE  Romantic Choreography (mp3cut.net).mp3")}
+      src={encodeURI("/background_music/Carl Timbol - Tahan (Official Lyric Visualizer).mp3")}
       loop
       preload="auto"
       // playsInline helps iOS treat this as inline media rather than requiring fullscreen behavior
